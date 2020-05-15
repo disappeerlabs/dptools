99814387f3cec1d1f46742465b601dd9768d208b
disappeerlabs <disappeerlabs@gmail.com>
2020-05-15 19:53:23 -0400

    Refactored test_keyring tests.

--- dptools/gpg/agents/tests/test_keyring.py
+++ dptools/gpg/agents/tests/test_keyring.py
@@ -9,22 +9,20 @@ License: GPLv3
 
 import unittest
 import gnupg
-from dptools.gpg.agents import gpgagent
-from dptools.gpg.agents import keyring
-from dptools.gpg.tests import basegpgtestclass
+from dptools.tests import mark
+from dptools.gpg.tests import helpers
+from dptools.gpg.agents import keyring, gpgagent
 
 
-class TestImports(unittest.TestCase):
+class TestKeyRingBasics(unittest.TestCase):
+
+    def setUp(self):
+        self.key_master = helpers.SetUpKeys()
+        self.keydir = self.key_master.alice_dir_path
+        self.k = keyring.KeyRing(self.keydir)
 
     def test_gpg_agent_import(self):
         self.assertEqual(gpgagent, keyring.gpgagent)
-
-
-class TestKeyRingBasics(basegpgtestclass.BaseGPGTestClass):
-
-    def setUp(self):
-        self.keydir = self.key_dir_path
-        self.k = keyring.KeyRing(self.keydir)
 
     def test_instance(self):
         self.assertIsInstance(self.k, keyring.KeyRing)
@@ -38,54 +36,15 @@ class TestKeyRingBasics(basegpgtestclass.BaseGPGTestClass):
     def test_gpg_attribute(self):
         self.assertIsInstance(self.k.gpg, gnupg.GPG)
 
-
-class TestKeyRingMethods(basegpgtestclass.BaseGPGTestClass):
-
-    def setUp(self):
-        self.keydir = self.key_dir_path
-        self.mal = {'type': 'pub',
-                    'trust': 'u',
-                    'length': '2048',
-                    'algo': '1',
-                    'keyid': '191E11551232B305',
-                    'date': '05/10/2020',
-                    'expires': '04/01/2030',
-                    'dummy': '',
-                    'ownertrust': 'u',
-                    'sig': '',
-                    'uids': ['mal (no comment) <mallory@none.com>'],
-                    'sigs': [],
-                    'subkeys': [['5A11342E5AB0DE01', 'e', '1DAF59C9E688299CA6A274A25A11342E5AB0DE01']],
-                    'fingerprint': '1AF427FA3F164D900D7B9913191E11551232B305'}
-        self.k = keyring.KeyRing(self.keydir)
-
     def test_attribute_get_key_list(self):
         name = 'get_raw_key_list'
         result = hasattr(self.k, name)
         self.assertTrue(result)
 
-    def test_get_key_list_returns_list(self):
-        result = self.k.get_raw_key_list()
-        self.assertIsInstance(result, gnupg.ListKeys)
-
-    def test_result_get_raw_key_list(self):
-        result = self.k.get_raw_key_list()
-        target_fingerprints = [d['fingerprint'] for d in result]
-        self.assertIn(self.mal['fingerprint'], target_fingerprints)
-
-    def test_result_get_raw_key_list_secret(self):
-        result = self.k.get_raw_key_list(secret=True)
-        final = result[0]
-        self.assertEqual(final['type'], 'sec')
-
     def test_attribute_export_method(self):
         name = 'export_key'
         check = hasattr(self.k, name)
         self.assertTrue(check)
-
-    def test_result_export_method_fingerprint_valid(self):
-        result = self.k.export_key(self.mal['fingerprint'])
-        self.assertIn("PUBLIC KEY BLOCK", result)
 
     def test_result_export_method_fingerprint_not_valid(self):
         result = self.k.export_key('XXX666')
@@ -96,14 +55,6 @@ class TestKeyRingMethods(basegpgtestclass.BaseGPGTestClass):
         check = hasattr(self.k, name)
         self.assertTrue(check)
 
-    def test_result_import_method_valid(self):
-        pub_key = self.k.export_key(self.mal['fingerprint'])
-        import tempfile
-        with tempfile.TemporaryDirectory() as temp_home_dir:
-            k = keyring.KeyRing(temp_home_dir)
-            result = k.import_key(pub_key)
-            self.assertTrue(result.count > 0)
-
     def test_result_import_method_not_valid(self):
         pub_key = 'xxx666'
         import tempfile
@@ -111,3 +62,44 @@ class TestKeyRingMethods(basegpgtestclass.BaseGPGTestClass):
             k = keyring.KeyRing(temp_home_dir)
             result = k.import_key(pub_key)
             self.assertTrue(result.count == 0)
+
+
+@unittest.skipIf(*mark.slow)
+class TestKeyRingMethods(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.key_master = helpers.SetUpKeys()
+        cls.key_master.set_up_alice()
+        cls.fingerprint = cls.key_master.alice_key['fingerprint']
+        cls.keydir = cls.key_master.alice_dir_path
+
+    def setUp(self):
+        self.key_master = helpers.SetUpKeys()
+        self.k = keyring.KeyRing(self.keydir)
+
+    def test_get_key_list_returns_list(self):
+        result = self.k.get_raw_key_list()
+        self.assertIsInstance(result, gnupg.ListKeys)
+
+    def test_result_get_raw_key_list(self):
+        result = self.k.get_raw_key_list()
+        target_fingerprints = [d['fingerprint'] for d in result]
+        self.assertIn(self.fingerprint, target_fingerprints)
+
+    def test_result_get_raw_key_list_secret(self):
+        result = self.k.get_raw_key_list(secret=True)
+        final = result[0]
+        self.assertEqual(final['type'], 'sec')
+
+    def test_result_export_method_fingerprint_valid(self):
+        result = self.k.export_key(self.fingerprint)
+        self.assertIn("PUBLIC KEY BLOCK", result)
+
+    def test_result_import_method_valid(self):
+        pub_key = self.k.export_key(self.fingerprint)
+        import tempfile
+        with tempfile.TemporaryDirectory() as temp_home_dir:
+            k = keyring.KeyRing(temp_home_dir)
+            result = k.import_key(pub_key)
+            self.assertTrue(result.count > 0)
