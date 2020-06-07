43cb0b6fb55e2978d6e495ea243780315bd44d64
disappeerlabs <disappeerlabs@gmail.com>
2020-06-06 20:26:51 -0400

    Extracted tests common to all concrete implementations of command pattern into a base test class. This class should be subclassed by all unit test case classes that test a specific command pattern implementation. Implemented base test class for sanity check command

--- dptools/commands/checksanity/tests/test_command_check_sanity.py
+++ dptools/commands/checksanity/tests/test_command_check_sanity.py
@@ -7,10 +7,7 @@ Copyright (C) 2020 Disappeer Labs
 License: GPLv3
 """
 
-import unittest
-from unittest.mock import MagicMock
-import queue
-from dptools.commands import abstracts
+from dptools.commands.tests import baseclasstestcommandpattern
 from dptools.commands import checksanity
 from dptools.commands.checksanity.checksanitycommand import (
     CheckSanityCommand,
@@ -19,54 +16,29 @@ from dptools.commands.checksanity.checksanitycommand import (
 )
 
 
-class TestBasicsAndIntegrationFlow(unittest.TestCase):
+class TestCheckSanityWithBaseClass(baseclasstestcommandpattern.BaseClassTestCommandPattern):
+
+    def config_register_callback(self):
+        return checksanity.register(self.mock_callback)
+
+    def config_command(self):
+        self.msg = "Hello there"
+        command = CheckSanityCommand(self.msg)
+        return command
+
+    def command_class(self):
+        return CheckSanityCommand
+
+    def handler_class(self):
+        return CheckSanityHandler
+
+    def result_class(self):
+        return CheckSanityResult
 
     def setUp(self):
-        self.queue = queue.Queue()
-        self.mock_callback = MagicMock()
-        self.mock_callback.handle = MagicMock()
-        # Register callback with command map
-        self.command_map = checksanity.register(self.mock_callback)
-        # Define a Command
-        self.msg = "Hello there"
-        self.command = CheckSanityCommand(self.msg)
+        super().setUp()
 
-    def test_check_sanity_command_instance(self):
-        self.assertTrue(issubclass(CheckSanityCommand, abstracts.AbstractCommand))
-
-    def test_check_sanity_handler_instance(self):
-        self.assertTrue(issubclass(CheckSanityHandler, abstracts.AbstractHandler))
-
-    def test_check_sanity_result_instance(self):
-        self.assertTrue(issubclass(CheckSanityResult, abstracts.AbstractResult))
-
-    def test_check_sanity_register_func_registers_callback_to_handle_method(self):
-        target_dict = checksanity.register(self.mock_callback)
-        self.assertEqual(target_dict[CheckSanityResult.__name__].handle, self.mock_callback)
-
-    def helper_get_result_from_queue(self):
-        # Grab the handler class from the map
-        handler_class = self.command_map[self.command.name]
-        # Initialize handler
-        handler_object = handler_class(self.queue)
-        # Run it
-        handler_object.handle(self.command)
-        # Get the result object, simulate polling queue
-        result_obj = self.queue.get()
-        return result_obj
-
-    def test_handling_command_from_command_map_returns_result(self):
+    def test_handling_command_from_command_map_returns_result_with_msg(self):
         result_obj = self.helper_get_result_from_queue()
-        # Results should be instance of result, generic to all commands
-        self.assertIsInstance(result_obj, CheckSanityResult)
         # Result should be original message, specific to this command
         self.assertEqual(result_obj.result, self.msg)
-
-    def test_handling_result_from_queue_calls_registered_callback(self):
-        result_obj = self.helper_get_result_from_queue()
-        # Get result handler, instantiate, call handle with result
-        result_handler_class = self.command_map[result_obj.name]
-        result_handler_object = result_handler_class(self.queue)
-        result_handler_object.handle(result_obj)
-        # Our mock function should now be called
-        self.assertTrue(self.mock_callback.called)
---
+++ dptools/commands/tests/baseclasstestcommandpattern.py
@@ -0,0 +1,82 @@
+"""
+baseclasstestcommandpattern.py
+
+Base Class for Setting Up Generic Tests for Any Command Pattern Implementation
+Subclass this for tests that are common to all command pattern implementations.
+
+Copyright (C) 2020 Disappeer Labs
+License: GPLv3
+"""
+
+import unittest
+from unittest.mock import MagicMock
+import queue
+from dptools.commands import abstracts
+import abc
+
+
+class BaseClassTestCommandPattern(unittest.TestCase, metaclass=abc.ABCMeta):
+
+    def setUp(self):
+        self.queue = queue.Queue()
+        self.mock_callback = MagicMock()
+        self.mock_callback.handle = MagicMock()
+        self.command_map = self.config_register_callback()
+        self.command = self.config_command()
+
+    @abc.abstractmethod
+    def config_register_callback(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def config_command(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def command_class(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def handler_class(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def result_class(self):
+        raise NotImplementedError
+
+    def test_command_instance(self):
+        self.assertTrue(issubclass(self.command_class(), abstracts.AbstractCommand))
+
+    def test_handler_instance(self):
+        self.assertTrue(issubclass(self.handler_class(), abstracts.AbstractHandler))
+
+    def test_result_instance(self):
+        self.assertTrue(issubclass(self.result_class(), abstracts.AbstractResult))
+
+    def test_register_func_registers_callback_to_handle_method(self):
+        self.assertEqual(self.command_map[self.result_class().__name__].handle, self.mock_callback)
+
+    def helper_get_result_from_queue(self):
+        # Grab the handler class from the map
+        handler_class = self.command_map[self.command.name]
+        # Initialize handler
+        handler_object = handler_class(self.queue)
+        # Run it
+        handler_object.handle(self.command)
+        # Get the result object, simulate polling queue
+        result_obj = self.queue.get()
+        return result_obj
+
+    def test_handling_command_from_command_map_returns_result(self):
+        result_obj = self.helper_get_result_from_queue()
+        # Results should be instance of result, generic to all commands
+        self.assertIsInstance(result_obj, self.result_class())
+
+    def test_handling_result_from_queue_calls_registered_callback(self):
+        result_obj = self.helper_get_result_from_queue()
+        # Get result handler, instantiate, call handle with result
+        result_handler_class = self.command_map[result_obj.name]
+        result_handler_object = result_handler_class(self.queue)
+        result_handler_object.handle(result_obj)
+        # Our mock function should now be called
+        self.assertTrue(self.mock_callback.called)
